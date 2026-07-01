7c0d787c6d760112fa6676d44bfd53e9546683ac
patchnote-mc <297409435+patchnote-mc@users.noreply.github.com>
2026-07-01 13:40:41 +0500

    Setup Submodules

--- fabric_decompiled/setup.sh
+++ fabric_decompiled/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/zsh
+set -e
 
 # fabric source is added as a submodule using command
 # git submodule add --depth 1 https://github.com/FabricMC/fabric-api.git fabric_decompiled/src
 
-# reloading
-git submodule update --init --recursive --depth 1
+URL="https://github.com/FabricMC/fabric-api.git"
+TAG="0.153.0+26.2"
 
-cd src
+# operate from the repo root
+cd "${0:A:h}/.."
 
-# check out the matching fabric-api tag
-git fetch origin tag "0.153.0+26.2" --no-tags
-git checkout "0.153.0+26.2"
+if git submodule status fabric_decompiled/src >/dev/null 2>&1; then
+  git submodule update --init --recursive --depth 1 fabric_decompiled/src
+else
+  rm -rf fabric_decompiled/src
+  git submodule add --force --depth 1 "$URL" fabric_decompiled/src
+fi
 
-cd ..
+# Check out the matching fabric-api tag.
+cd fabric_decompiled/src
+git fetch --depth 1 origin tag "$TAG" --no-tags
+git -c advice.detachedHead=false checkout "$TAG"
--- mc_decompiled/setup.sh
+++ mc_decompiled/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+set -e
 
 # set version here
 VERSION="26.2"
@@ -6,13 +7,20 @@ VERSION="26.2"
 # mc decompiled sources are added as a submodule using command
 # git submodule add --depth 1 -b "$VERSION" git@github-patch:patchnote-mc/mc_decompiled.git mc_decompiled/sources/$VERSION
 
-# reloading
-git submodule update --init --recursive --depth 1
+URL="git@github-patch:patchnote-mc/mc_decompiled.git"
+SUB="mc_decompiled/sources/$VERSION"
 
-cd "sources/$VERSION"
+# operate from the repo root
+cd "${0:A:h}/.."
 
-# check out the matching version branch
-git fetch origin "$VERSION" --depth 1
-git checkout "$VERSION"
+if git submodule status "$SUB" >/dev/null 2>&1; then
+  git submodule update --init --recursive --depth 1 "$SUB"
+else
+  rm -rf "$SUB"
+  git submodule add --force --depth 1 -b "$VERSION" "$URL" "$SUB"
+fi
 
-cd ../..
+# Check out the matching version branch.
+cd "$SUB"
+git fetch --depth 1 origin "$VERSION"
+git -c advice.detachedHead=false checkout "$VERSION"
